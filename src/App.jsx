990e2d32ad4d0b7b2f5b0e42f0f68fcda4c4ca0e
Zakariaaben <zakaria.benhamiche@gmail.com>
2024-05-24 21:45:20 +0100

    Adding Multi Tree View

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,82 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
+
 import './App.css';
 import Graphviz from 'graphviz-react';
-import { useRef } from 'react';
+
 import { Circles } from 'react-loader-spinner';
+import { ErrorBoundary } from 'react-error-boundary';
 
 function App() {
   const [dot, setDot] = useState('graph{a--b}');
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const myreef = useRef()
+  const [bst,setBST]  = useState(1);
+  
+  
 
-  async function fetchData() {
+  async function fetchData(num) {
     try {
-      const response = await fetch('https://graph-tp.vercel.app/data');
+      console.log(num);
+
+      const url = `https://testing111.vercel.app/data/${num}`;
+      setLoading(true);
+      const response = await fetch(url);
+      
       
       if (!response.ok) {
 
         throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
       }
       const data = await response.text();
+      
       setDot(data);
       
     } catch (error) {
-      console.error('Error fetching data:', error);
+      
       setError(error);
     } finally {
-      setLoading(false);
+      setLoading(false)
+      
     }
   }
 
-  useEffect(() => {
-    fetchData();
-    // Set up the interval to fetch data every 10 seconds
-    const interval = setInterval(() => {
-      fetchData();
-    }, 1000); // 10000 milliseconds = 10 seconds
 
-    // Clean up the interval on unmount
-    return () => clearInterval(interval);
-  }, []);
+  useEffect(()=>{
+    
+      fetchData(bst);
+    
+    
+
+  },[bst]);
   
 
-  if (loading) {
-    return <div className="container">
+  
+    
+
+  if (error!=null) {
+    return <div className='container'  style={{fontSize:"3rem"}}> Please Lunch the C program before </div>;
+  }
+  
+
+ 
+ const bstlinks = ()=>{
+  const elements = []; // Initialize an empty array to hold the div elements
+  
+  for (let i = 1; i < 11; i++) {
+    elements.push(<div key={i} onClick={()=>{setBST(i)}} style={{fontSize:"1.2rem",cursor:"pointer"}}>Arbre {i}</div>); // Push a new div element to the array
+  }
+  return elements;
+ }
+  
+
+  
+
+  return (
+    <>
+      <div className='container'>
+        <div className='links'style={{display:"flex",justifyContent:"space-between",width:"80%",padding:"60px"}}>{bstlinks()}</div>
+          <h2 style={{marginBottom:"20px",textDecoration:"underline",fontSize:"2rem"}}>Arbre <span style={{color:"red"}}> {bst}</span> :</h2>
+         { loading? <div className="container">
       <Circles
       height="80"
         width="80"
@@ -53,22 +86,15 @@ function App() {
         wrapperClass=""
         visible={true}
         />
-    </div>;
-  }
-
-  if (error) {
-    return <div> Please Lunch the C program before </div>;
-  }
-  if(dot == "Initial Content") {
-    return <div style={{fontSize:"3rem"}}>Please Lunch the C program before</div>;
-  }
-
-  
-
-  return (
-    <>
-      <div className='container'  >
-        <Graphviz dot={dot}   options={{ height:"100%",width:"900px"}}/>
+    </div>:
+           
+              <Graphviz key={dot} dot={dot} options={{ height: '100%', width: '900px' }}  />
+             
+          }
+          <p style={{padding:"10px"}}>&copy; All rights reserved Zakaria Benhamiche </p>
+          
+            
+        
       </div>
     </>
   );
